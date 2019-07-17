f4c7796a7bc91e4a295eb2a84083a84eda041751
Sarah_Abanumay <sara-95-@outlook.sa>
2019-07-17 15:17:10 +0300

    Add files via upload

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import json
 import plotly
+import numpy as np
 import pandas as pd
 
 from nltk.stem import WordNetLemmatizer
@@ -7,7 +8,7 @@ from nltk.tokenize import word_tokenize
 
 from flask import Flask
 from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
+from plotly.graph_objs import Bar, Pie
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
 
@@ -24,28 +25,63 @@ def tokenize(text):
         clean_tokens.append(clean_tok)
 
     return clean_tokens
-
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('labeled_messages', engine)
+engine.dispose()
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/disaster_model.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
 def index():
-    
+
     # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
-    
+
+    original_counts = [df.original.notnull().sum(), df.original.isnull().sum()]
+    original_names = ['Translated', 'Not Translated']
+
+    lengths = df.message.str.split().str.len()
+    length_counts, length_division = np.histogram(lengths,
+                                              range=(0, lengths.quantile(0.99)))
+
     # create visuals
-    # TODO: Below is an example - modify to create your own visuals
     graphs = [
+        {
+            'data': [
+                Bar(
+                    x=length_division,
+                    y=length_counts
+                    )
+            ],
+
+            'layout': {
+                'title': 'Message Length Distribution',
+                },
+            'yaxis': {
+                'title': "Count"
+            },
+            'xaxis': {
+                'title': "Message Length"
+            }
+        },
+        {
+            'data': [
+                Pie(
+                    labels=original_names,
+                    values=original_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Percentage of Translated Messages',
+            }
+        },
         {
             'data': [
                 Bar(
@@ -65,26 +101,26 @@ def index():
             }
         }
     ]
-    
+
     # encode plotly graphs in JSON
-ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
-graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
+    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+
     # render web page with plotly graphs
-return render_template('master.html', ids=ids, graphJSON=graphJSON)
+    return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
 
 # web page that handles user query and displays model results
 @app.route('/go')
 def go():
     # save user input in query
-    query = request.args.get('query', '') 
+    query = request.args.get('query', '')
 
     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
     classification_results = dict(zip(df.columns[4:], classification_labels))
 
-    # This will render the go.html Please see that file. 
+    # This will render the go.html Please see that file.
     return render_template(
         'go.html',
         query=query,
